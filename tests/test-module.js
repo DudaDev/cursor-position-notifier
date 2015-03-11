42eb6d148f54260be3d7cedd2a7b0874b91cea6a
ramyb <rami@dudamobile.com>
2015-03-11 14:52:57 +0200

    fix missing enter/leave events, bump

--- tests/test-module.js
+++ tests/test-module.js
@@ -1,5 +1,6 @@
 "use strict";
-function CursorPositionNotifier(options){
+
+function CursorPositionNotifier(options) {
 	var self = this;
 	options = options || {};
 	this.containerSelector = options.containerSelector;
@@ -11,48 +12,41 @@ function CursorPositionNotifier(options){
 	this.debounceDelay = options.debounceDelay || 100;
 	this.isActive = false;
 	this._current = {};
-	(function(){
+	(function() {
 		var __current = {
 			column: 0,
 			row: 0,
 			cursorOn: null
 		};
 		Object.defineProperty(this._current, 'observed', {
-			set: function(newCurrent){
+			set: function(newCurrent) {
 				newCurrent = _.extend({}, __current, newCurrent);
-				if (!newCurrent.cursorOn){
+				if (!newCurrent.cursorOn) {
 					newCurrent.row = null;
 					newCurrent.column = null;
 				}
-				if (
-					!(
-						!newCurrent.cursorOn &&
+				if (!(!newCurrent.cursorOn &&
 						typeof newCurrent.row === 'number' &&
-						typeof newCurrent.column === 'number'	
-					)
-					&&
+						typeof newCurrent.column === 'number'
+					) &&
 					!(
-						newCurrent.cursorOn
-						&&
+						newCurrent.cursorOn &&
 						(
 							typeof newCurrent.row !== 'number' ||
-							typeof newCurrent.column !== 'number'			
+							typeof newCurrent.column !== 'number'
 						)
-					)
-					&&
+					) &&
 					!(
-						(typeof newCurrent.row === 'number' && newCurrent.row < 0 )
-						||
-						(typeof newCurrent.column === 'number' && newCurrent.column < 0 )
-					)
-					&&
+						(typeof newCurrent.row === 'number' && newCurrent.row < 0) ||
+						(typeof newCurrent.column === 'number' && newCurrent.column < 0)
+					) &&
 					(
 						newCurrent.cursorOn !== __current.cursorOn ||
 						newCurrent.row !== __current.row ||
 						newCurrent.column !== __current.column
 					)
-				){
-					__current = newCurrent;	
+				) {
+					__current = newCurrent;
 					self.callback({
 						cursorOn: __current.cursorOn,
 						row: __current.row,
@@ -63,32 +57,33 @@ function CursorPositionNotifier(options){
 				}
 
 			},
-			get: function(){
+			get: function() {
 				return __current;
 			}
 		});
 	}.bind(this))();
-	
+
 };
 
-CursorPositionNotifier.prototype.start = function(){
+CursorPositionNotifier.prototype.start = function() {
 	var self = this;
 	this.isActive = true;
 	$('body').on(
 		'mouseenter.CursorPositionNotifier mouseleave.CursorPositionNotifier',
-	 	this.containerSelector + ' ' + this.commonSelector,
-	 	_.debounce(this._onElementSwitch.bind(this), this.debounceDelay));
+		this.containerSelector + ' ' + this.commonSelector,
+		this._onElementSwitch.bind(this)
+	);
 };
 
-CursorPositionNotifier.prototype._onElementSwitch = function(event){
+CursorPositionNotifier.prototype._onElementSwitch = function(event) {
 	var parsed;
 	if (this.isActive) {
 		parsed = this._parseEvent(event);
-		this._initCurrentTracking(parsed);
+		this._initCurrentTracking(parsed, event);
 	}
 };
 
-CursorPositionNotifier.prototype._parseEvent = function(event){
+CursorPositionNotifier.prototype._parseEvent = function(event) {
 	var attrVal,
 		path = [],
 		$toElement = $(event.toElement),
@@ -99,21 +94,21 @@ CursorPositionNotifier.prototype._parseEvent = function(event){
 			height: null,
 			width: null
 		};
-	if ($toElement.length > 0 && $toElement.is(this.commonSelector)){
+	if ($toElement.length > 0 && $toElement.is(this.commonSelector)) {
 		attrVal = $toElement.attr(this.identifierAttribute);
 		$currentElement = $toElement;
 		path = [];
-		
+
 		while ($currentElement.length) {
 			path.push($currentElement.attr(this.identifierAttribute));
 			$currentElement = $currentElement.parent().closest(this.commonSelector);
 		}
 
-		path = path.reverse();	
+		path = path.reverse();
 	}
 
 	isValidElement = $toElement.length && attrVal;
-	if (isValidElement){
+	if (isValidElement) {
 		boundingRect = $toElement.get(0).getBoundingClientRect();
 		rect.height = boundingRect.height;
 		rect.width = boundingRect.width;
@@ -127,20 +122,21 @@ CursorPositionNotifier.prototype._parseEvent = function(event){
 	};
 };
 
-CursorPositionNotifier.prototype._initCurrentTracking = function(info){
+CursorPositionNotifier.prototype._initCurrentTracking = function(info, event) {
 	var element = info.element;
 
-	if (this._current.observed.element){
+	if (this._current.observed.element) {
 		this._current.observed.element.off('mousemove.cursorPositionNotifier');
 	}
 	this._current.observed = info;
-	if (info.cursorOn){
-		element.on('mousemove.cursorPositionNotifier',_.debounce(this._onCurrentMousemove.bind(this), this.debounceDelay / 2));	
+	if (info.cursorOn) {
+		this._onCurrentMousemove(event);
+		element.on('mousemove.cursorPositionNotifier', _.debounce(this._onCurrentMousemove.bind(this), this.debounceDelay));
 	}
-	
+
 };
 
-CursorPositionNotifier.prototype.pause = function(){
+CursorPositionNotifier.prototype.pause = function() {
 	this.isActive = false;
 	$('body').off('mouseenter.cursorPositionNotifier mouseleave.cursorPositionNotifier');
 };
@@ -158,7 +154,7 @@ CursorPositionNotifier.prototype._onCurrentMousemove = function(event) {
 	};
 };
 
-CursorPositionNotifier.prototype._computeGridPosition = function(position){
+CursorPositionNotifier.prototype._computeGridPosition = function(position) {
 	var gridPosition = {};
 	gridPosition.row = Math.floor(position.y / Math.floor(this._current.observed.height / this.rows));
 	gridPosition.column = Math.floor(position.x / Math.floor(this._current.observed.width / this.columns));
@@ -167,7 +163,7 @@ CursorPositionNotifier.prototype._computeGridPosition = function(position){
 
 CursorPositionNotifier.prototype.resume = CursorPositionNotifier.prototype.start;
 
-CursorPositionNotifier.prototype.destroy = function(){
+CursorPositionNotifier.prototype.destroy = function() {
 	this.hold();
 	this.callback = null;
 };
